06b35d46dbdf529ffb433927f2ebf6bba9c1e2be
zhangxin <ZYL19820321@126.com>
2019-03-06 19:35:29 +0800

    首次提交到本地

---
+++ demo1/src/test/java/test/NewTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  
+	  System.out.println("©ирт");
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
